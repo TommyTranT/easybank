a2b50ac89d037145e15ee1cea64945e08d1f6529
Tommy Tran <haitran1995@gmail.com>
2023-02-05 00:30:13 -0500

    add btnHamburger id to open menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,15 @@
+// when container with id 'btnHamburger' is clicked,
+// 1. if it contains class 'open', remove it
+// 2. else, add it
 
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+
+  if (btnHamburger.classList.contains("open")) {
+    btnHamburger.classList.remove("open");
+  } else {
+    btnHamburger.classList.add("open");
+  }
+});
