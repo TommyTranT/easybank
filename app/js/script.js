f8788e6a3f82e372f4d8afcf7a7859e4c5f1f38d
Tommy Tran <haitran1995@gmail.com>
2023-04-05 02:19:24 -0400

    fix: hamburger menu bug

--- app/js/script.js
+++ app/js/script.js
@@ -6,7 +6,7 @@ const btnHamburger = document.querySelector("#btnHamburger");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
-const fadeElems = document.querySelectorAll(".has-fade"); //querySelectorAll gets all the html using the class name
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("click hamburger");
@@ -15,7 +15,7 @@ btnHamburger.addEventListener("click", function () {
     // Close Hamburger Menu
     body.classList.remove("noscroll");
     header.classList.remove("open");
-    fadeElems.forEach((element) => {
+    fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
@@ -23,7 +23,7 @@ btnHamburger.addEventListener("click", function () {
     // Open Hamburger Menu
     body.classList.add("noscroll");
     header.classList.add("open");
-    fadeElems.forEach((element) => {
+    fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
       element.classList.add("fade-in");
     });
--- personal_notes.md
+++ personal_notes.md
@@ -1,6 +1,6 @@
 https://www.youtube.com/watch?v=aoQ6S1a32j8
 
-3:12:10
+3:19:00
 
 * If you need to add flex to everything, just make it a class
 
